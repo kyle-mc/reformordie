cb8d668ea8b35038e0e9880a3ff2bbf1c0767f6e
Kyle <kylenyc11@gmail.com>
2026-03-31 17:10:45 -0500

    ROD-16
    Google tag manager and google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import PageNotFound from './lib/PageNotFound';
+
+function RouteTracker() {
+  const location = useLocation();
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'virtualPageview',
+      pageUrl: location.pathname,
+    });
+  }, [location]);
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <RouteTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<PageNotFound />} />
